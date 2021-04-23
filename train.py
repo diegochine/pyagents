2b3965a05abe635200c9fbb5d008c758a75b251f
diegochine <chine.diego@gmail.com>
2021-04-23 18:36:43 +0200

    refactoring

--- train.py
+++ train.py
@@ -14,25 +14,31 @@ if not os.path.exists(cfg.OUTPUT_DIR):
 
 
 agent = DQNAgent(state_size, action_size)
+# agent.load(cfg.OUTPUT_DIR + '/endrun.hdf5')
 
 for episode in range(cfg.N_EPISODES):
 
     state = env.reset()
     state = np.reshape(state, (1, agent.state_shape))
+    step = 0
+    done = False
 
-    for step in range(1000):
+    while not done and step < 1000:
         # env.render()
         action = agent.act(state)
         next_state, reward, done, info = env.step(action)
-        reward = reward if not done else -10
+        reward = reward if not done else -100
         next_state = np.reshape(next_state, (1, agent.state_shape))
         agent.remember(state, action, reward, next_state, done)
         state = next_state
 
         if done:
-            print(f'EPISODE: {episode:4d}/{cfg.N_EPISODES:4d}, score: {step:4d}')
-            break
+            print(f'EPISODE: {episode:4d}/{cfg.N_EPISODES:4d}, SCORE: {step:4d}, EPS: {agent.epsilon}')
+        step += 1
 
     agent.replay(cfg.BATCH_SIZE)
-    if (episode % 50) == 0:
+
+    if (episode % 1000) == 0:
         agent.save(cfg.OUTPUT_DIR + f"/ep{episode}.hdf5")
+
+agent.save(cfg.OUTPUT_DIR + "endrun.hdf5")
