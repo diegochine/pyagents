6ffbc1a136e8fc670385be256f5f22f733ecad1d
diegochine <chine.diego@gmail.com>
2021-04-23 18:34:28 +0200

    added regularization

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+from keras.regularizers import l1_l2, l2
 import config as cfg
 
 
@@ -11,7 +12,7 @@ class DQNAgent:
 
     def __init__(self, state_shape, action_shape, gamma=cfg.GAMMA, learning_rate=cfg.LEARNING_RATE,
                  epsilon=cfg.EPSILON, epsilon_decay=cfg.EPSILON_DECAY, epsilon_min=cfg.EPSILON_MIN,
-                 memory_size=cfg.MEMORY_SIZE):
+                 memory_size=cfg.MEMORY_SIZE, min_memories=cfg.MIN_MEMORIES):
         self.state_shape = state_shape
         self.action_shape = action_shape
 
@@ -21,14 +22,22 @@ class DQNAgent:
         self.epsilon_decay = epsilon_decay
         self.epsilon_min = epsilon_min
         self.learning_rate = learning_rate
+        self.min_memories = min_memories
 
         self.model = self._build_model()
 
     def _build_model(self):
         model = Sequential()
 
-        model.add(Dense(24, input_dim=self.state_shape, activation='relu'))
-        model.add(Dense(24, activation='relu'))
+        model.add(Dense(64, activation='relu', input_dim=self.state_shape,
+                        kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4),
+                        bias_regularizer=l2(1e-4)))
+        model.add(Dense(32, activation='relu',
+                        kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4),
+                        bias_regularizer=l2(1e-4)))
+        model.add(Dense(16, activation='relu',
+                        kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4),
+                        bias_regularizer=l2(1e-4)))
         # output layer, linear because the output is
         model.add(Dense(self.action_shape, activation='linear'))
 
@@ -65,7 +74,7 @@ class DQNAgent:
         :param batch_size:
         :return:
         """
-        if len(self.memory) > batch_size:
+        if len(self.memory) > self.min_memories:
             minibatch = sample(self.memory, batch_size)
 
             for state, action, reward, next_state, done in minibatch:
