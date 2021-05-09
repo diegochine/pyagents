dfadd0670ecc29a7a172b012bfc6abef1d8fced5
Giorgia Campardo <giorgiac98.gc@gmail.com>
2021-05-09 23:23:28 +0200

    encoding network

--- networks/encoding_network.py
+++ networks/encoding_network.py
@@ -13,30 +13,67 @@ from networks import Network
 class EncodingNetwork(Network):
 
     def __init__(self, state_shape,
-                 conv_layer_params=None, fc_layer_params=None, activation=relu,
-                 name='EncodingNetwork'):
+                 conv_layer_params=None, fc_layer_params=None, activation=relu, dtype=tf.float32,
+                 name='EncodingNetwork', conv_type='2d'):
         super().__init__(name=name)
-        self.model = self._build_model()
 
-    def _build_model(self):
-        inputs = Input(shape=self.state_shape)
-        x = Dense(16, activation='relu',
-                  kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4),
-                  bias_regularizer=l2(1e-4))(inputs)
-        x = Dense(32, activation='relu',
-                  kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4),
-                  bias_regularizer=l2(1e-4))(x)
-        outputs = Dense(16, activation='relu',
-                        kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4),
-                        bias_regularizer=l2(1e-4))(x)
+        kernel_initializer = tf.keras.initializers.VarianceScaling(
+            scale=2.0, mode='fan_in', distribution='truncated_normal')
 
-        model = Model(inputs=inputs, outputs=outputs)
-        model.compile(loss='mse', optimizer=Adam(self.learning_rate))
+        layers = []
 
-        return model
+        if conv_layer_params:
+            if conv_type == '2d':
+                conv_layer_type = tf.keras.layers.Conv2D
+            elif conv_type == '1d':
+                conv_layer_type = tf.keras.layers.Conv1D
+            else:
+                raise ValueError('unsupported conv type of %s. Use 1d or 2d' % (
+                    conv_type))
+
+            for config in conv_layer_params:
+                if len(config) == 4:
+                    (filters, kernel_size, strides, dilation_rate) = config
+                elif len(config) == 3:
+                    (filters, kernel_size, strides) = config
+                    dilation_rate = (1, 1) if conv_type == '2d' else (1,)
+                else:
+                    raise ValueError(
+                        'only 3 or 4 elements permitted in conv_layer_params tuples')
+                layers.append(
+                    conv_layer_type(
+                        filters=filters,
+                        kernel_size=kernel_size,
+                        strides=strides,
+                        dilation_rate=dilation_rate,
+                        activation=activation,
+                        kernel_initializer=kernel_initializer,
+                        dtype=dtype))
+
+        layers.append(tf.keras.layers.Flatten())
+
+        if fc_layer_params:
+            for num_units in fc_layer_params:
+                kernal_regularizer = None # if necessary sholud have wheight decay param as in tf
+                layers.append(
+                    tf.keras.layers.Dense(
+                        num_units,
+                        activation=activation,
+                        kernel_initializer=kernel_initializer,
+                        kernel_regularizer=kernal_regularizer,
+                        dtype=dtype))
+
+        # Pull out the nest structure of the preprocessing layers. This avoids
+        # saving the original kwarg layers as a class attribute which Keras would
+        # then track.
+        self._postprocessing_layers = layers
+        self.built = True  # Allow access to self.variables
 
     def call(self, inputs, training=False, mask=None):
-        return self.model(inputs)
+        states = inputs
+        for layer in self._postprocessing_layers:
+            states = layer(states, training=training)
+        return states
 
     def get_config(self):
         pass
