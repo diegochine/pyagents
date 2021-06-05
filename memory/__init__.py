7d9ad19d3384abf3586bd5a223e719b1dd80061a
diegochine <chine.diego@gmail.com>
2021-06-05 20:02:38 +0200

    refactoring

--- memory/__init__.py
+++ memory/__init__.py
@@ -1,3 +1,3 @@
-from memory.buffer import Buffer
+from memory.buffer import Buffer, load_memories
 from memory.uniformbuffer import UniformBuffer
 from memory.prioritizedbuffer import PrioritizedBuffer
