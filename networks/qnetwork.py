9d3508f654e618d83d9e02c40d780e5bcda43e74
diegochine <chine.diego@gmail.com>
2021-06-07 23:11:57 +0200

    this commit doesn't exist

--- networks/qnetwork.py
+++ networks/qnetwork.py
@@ -15,7 +15,7 @@ class QNetwork(Network):
         self._config = {'state_shape': state_shape,
                         'action_shape': action_shape,
                         'preprocessing_layers': [lay.get_config() for lay in preprocessing_layers]
-                                                 if preprocessing_layers else [],
+                        if preprocessing_layers else [],
                         'conv_layer_params': conv_layer_params if conv_layer_params else [],
                         'fc_layer_params': fc_layer_params if fc_layer_params else [],
                         'activation': activation,
