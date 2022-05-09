fixed return normalization

pyagents/agents/agent.py
@@ -35,7 +35,6 @@ class Agent(tf.Module, abc.ABC):
                  state_shape: tuple,
                  action_shape: tuple,
                  training: bool,
-                 rew_gamma: float = 0.0,
                  save_dir: str = './output',
                  save_memories: bool = False,
                  log_gradients: bool = False,
@@ -45,8 +44,6 @@ class Agent(tf.Module, abc.ABC):
         Args:
             state_shape: Tuple representing the shape of the input space (observations).
             action_shape: Tuple representing the shape of the action space.
-            rew_gamma; (Optional) if bigger than 0.0 rewards are normalized s.t. exponential moving average with
-              discount rew_gamma is fixed. Defaults to 0.0.
             training: (Optional) If True, agent is in training phase. Defaults to True.
             name: (Optional) Name of the agent.
         """
@@ -66,10 +63,6 @@ class Agent(tf.Module, abc.ABC):
                         'name': name}
         self._normalizers = dict()
         self.init_normalizer('obs', shape=self._state_shape)
-        if rew_gamma > 0:
-            self.init_normalizer('rewards', shape=(1,))
-        self._rew_ema = np.zeros(1,)
-        self._rew_gamma = rew_gamma
         self._train_step = 0
         self.num_envs = 0
         self._log_gradients = log_gradients
@@ -188,10 +181,11 @@ class Agent(tf.Module, abc.ABC):
         self._log_dict = {}
         self.is_logging = True
 
-    def init(self, envs, *args, **kwargs):
+    def init(self, envs, env_config=None, *args, **kwargs):
         """Performs additional initialization, such as collecting memories for off-policy agents"""
         self.num_envs = getattr(envs, "num_envs", 1)
-        self._rew_ema = np.zeros(self.num_envs)
+        if self._wandb_run is not None and env_config is not None:
+            self._wandb_run.config.update(env_config)
 
     def act(self, state: np.ndarray, mask: Optional[np.ndarray] = None):
         """Returns the best action in the state according to current policy.
@@ -258,7 +252,7 @@ class Agent(tf.Module, abc.ABC):
                  *args, **kwargs) -> None:
         """Stores piece of experience in internal memory/buffer.
 
-        Subclasses are expected to implement this method, and call it if reward normalization is needed.
+        Subclasses are expected to implement this method..
         Args:
             done:
             state: current state s_t
@@ -266,9 +260,7 @@ class Agent(tf.Module, abc.ABC):
             reward: reward r_(t+1) obtained by the agent following the execution of a_t in s_t
             next_state: state s_(t+1)
         """
-        if self._rew_gamma > 0:
-            self._rew_ema = self._rew_ema * self._rew_gamma + reward
-            self.update_normalizer('rewards', self._rew_ema)
+        pass
 
     def _wandb_define_metrics(self):
         """Defines WandB metrics.

pyagents/agents/on_policy_agent.py
@@ -15,7 +15,6 @@ class OnPolicyAgent(Agent, ABC):
                  state_shape: tuple,
                  action_shape: tuple,
                  training: bool,
-                 rew_gamma: float = 0.0,
                  lam_gae: float = 0.9,
                  save_dir: str = './output',
                  save_memories: bool = False,
@@ -23,10 +22,10 @@ class OnPolicyAgent(Agent, ABC):
         super(OnPolicyAgent, self).__init__(state_shape,
                                             action_shape,
                                             training,
-                                            rew_gamma=rew_gamma,
                                             save_dir=save_dir,
                                             save_memories=save_memories,
                                             name=name)
+        self._config.update({'lam_gae': lam_gae})
         self._memory = {k: None for k in self.MEMORY_KEYS}  # keeps track of current trajectories
         self._rollout_size = None  # size of above container, #steps * #envs
         self._step = 0  # step for indexing above memory
@@ -36,9 +35,9 @@ class OnPolicyAgent(Agent, ABC):
     def on_policy(self):
         return True
 
-    def init(self, envs, rollout_steps, *args, **kwargs):
+    def init(self, envs, rollout_steps, env_config=None, *args, **kwargs):
         """Initializes memory (i.e. trajectories storage)."""
-        super().init(envs, *args, **kwargs)
+        super().init(envs, env_config=env_config, *args, **kwargs)
         assert isinstance(envs, gym.vector.VectorEnv), 'envs must be instance of VectorEnv even for a single instance'
         if isinstance(envs.single_action_space, gym.spaces.Discrete):
             self._memory['actions'] = np.zeros((rollout_steps, envs.num_envs))
@@ -80,9 +79,6 @@ class OnPolicyAgent(Agent, ABC):
         super().remember(state, action, reward, next_state, done, *args, **kwargs)
         self._memory['states'][self._step] = state
         self._memory['actions'][self._step] = action
-        if 'rewards' in self.normalizers:
-            _, std_dev = self.get_normalizer('rewards')
-            reward = reward / std_dev
         self._memory['rewards'][self._step] = reward
         self._memory['next_states'][self._step] = next_state
         self._memory['dones'][self._step] = done
@@ -101,7 +97,7 @@ class OnPolicyAgent(Agent, ABC):
         returns = tf.convert_to_tensor(returns, dtype=tf.float32)
         return tf.reshape(returns, (-1, 1))
 
-    def compute_gae(self, state_values, next_state_values) -> tf.Tensor:
+    def compute_gae(self, state_values, next_state_values):
         """Computes Generalized Advantage Estimation of current trajectory.
 
         Args:
@@ -112,12 +108,17 @@ class OnPolicyAgent(Agent, ABC):
         next_state_values = next_state_values.numpy()
         rewards = np.reshape(self._memory['rewards'], -1)
         dones = 1 - np.reshape(self._memory['dones'], -1)
-        advantages = np.zeros(len(rewards))
+        advantages = np.zeros_like(state_values)
+
         advantages[-1] = rewards[-1] + (self.gamma * dones[-1] + next_state_values[-1]) - state_values[-1]
         for t in reversed(range(len(rewards) - 1)):
             v_t = state_values[t]
             v_tp1 = next_state_values[t]
-            delta = rewards[t] + (self.gamma * dones[t] + v_tp1) - v_t
+            delta = rewards[t] + (self.gamma * dones[t] * v_tp1) - v_t
             advantages[t] = delta + (self.gamma * self._lam_gae * advantages[t + 1] * dones[t])
+
+        returns = tf.convert_to_tensor(advantages + state_values, dtype=tf.float32)
         advantages = tf.convert_to_tensor(advantages, dtype=tf.float32)
-        return tf.stop_gradient(tf.reshape(advantages, (-1, 1)))
+        # normalization taken care by the underlying algo
+        # stop gradient to avoid backpropagating through the advantage estimator
+        return tf.stop_gradient(returns), tf.stop_gradient(advantages)

pyagents/agents/ppo.py
@@ -11,7 +11,7 @@ from pyagents.utils import types
 
 @gin.configurable
 class PPO(OnPolicyAgent):
-    """An agent implementing the REINFORCE algorithm, also known as Vanilla Policy Gradient."""
+    """An agent implementing the PPO algorithm"""
 
     def __init__(self,
                  state_shape: tuple,
@@ -26,16 +26,16 @@ class PPO(OnPolicyAgent):
                  standardize: bool = True,
                  lam_gae: float = 0.9,
                  target_kl: float = 0.01,
+                 returns_normalization: bool = True,
                  critic_value_coef: types.Float = 0.5,
                  entropy_coef: types.Float = 0,
                  gradient_clip_norm: Optional[float] = 0.5,
                  training: bool = True,
-                 rew_gamma: float = 0.0,
                  save_dir: str = './output',
                  log_dict: dict = None,
                  name: str = 'PPO',
                  wandb_params: Optional[dict] = None):
-        """Creates a VPG agent.
+        """Creates an PPO agent.
 
                 Args:
                     state_shape: Tuple representing the shape of the input space (observations).
@@ -48,9 +48,12 @@ class PPO(OnPolicyAgent):
                     critic_value_coef: (Optional) Coefficient applied on the value function during loss computation.
                     policy: (Optional) The policy the agent should follow. Defaults to policy network's policy.
                     gamma: (Optional) Discount factor. Defaults to 0.5.
-                    standardize: (Optional) If True, standardizes delta before computing gradient update.
-                      Defaults to False.
+                    standardize: (Optional) If True, standardizes advantages at the minibatch level.
+                      Defaults to True.
                     lam_gae: (Optional) Lambda for Generalized Advantage Estimation. Defaults to 0.9.
+                    target_kl: (Optional) Target KL divergence for early stopping condition (policy network)
+                      Defaults to 0.01.
+                    returns_normalization: (Optional) If True, enables return normalization. Defaults to True.
                     entropy_coef: (Optional) Coefficient applied to entropy loss. Defaults to 1e-3.
                     gradient_clip_norm: (Optional) Global norm for gradient clipping, pass None to disable.
                       Defaults to 0.5.
@@ -60,7 +63,7 @@ class PPO(OnPolicyAgent):
                     name: (Optional) Name of the agent.
                     wandb_params: (Optional) Dict of parameters to enable WandB logging. Defaults to None.
                 """
-        super(PPO, self).__init__(state_shape, action_shape, rew_gamma=rew_gamma, lam_gae=lam_gae,
+        super(PPO, self).__init__(state_shape, action_shape, lam_gae=lam_gae,
                                   training=training, save_dir=save_dir, name=name)
         if actor_opt is None and training:
             raise ValueError('agent cannot be trained without optimizer')
@@ -82,6 +85,8 @@ class PPO(OnPolicyAgent):
         self._gradient_clip_norm = gradient_clip_norm
         self._clip_eps = clip_eps
         self._target_kl = target_kl
+        if returns_normalization:
+            self.init_normalizer('returns', (1,))
 
         self._train_step_pi = 0
         self._train_step_v = 0
@@ -154,7 +159,6 @@ class PPO(OnPolicyAgent):
         pi_losses, v_losses, e_losses = [], [], []
         # update normalizers, reshape to remove batch dimension(s)
         states = self.normalize('obs', np.reshape(self._memory['states'], (self._rollout_size, -1)))
-        # self.update_normalizer('rewards', np.reshape(self._memory['rewards'], -1))
         # convert inputs to tf tensors
         states = tf.convert_to_tensor(states, dtype=tf.float32)
         actions = tf.convert_to_tensor(self._memory['actions'], dtype=tf.float32)
@@ -168,15 +172,22 @@ class PPO(OnPolicyAgent):
         for _ in range(update_rounds):
             # compute gae and td(lambda) returns, once per data pass
             state_values = self._vf(states).critic_values
-
-            next_states = tf.convert_to_tensor(self._memory['next_states'], dtype=tf.float32)
+            next_states = self.normalize('obs', np.reshape(self._memory['next_states'], (self._rollout_size, -1)))
+            next_states = tf.convert_to_tensor(next_states, dtype=tf.float32)
             next_states = tf.reshape(next_states, (self._rollout_size, -1))
             next_state_values = self._vf(next_states).critic_values
-            adv = self.compute_gae(state_values=state_values, next_state_values=next_state_values)
-            returns = tf.stop_gradient(adv + state_values)
 
-            adv = tf.reshape(adv, (self._rollout_size, 1))
-            returns = tf.reshape(returns, (self._rollout_size, 1))
+            if 'returns' in self.normalizers:  # unnormalize value function predictions
+                _, ret_std = self.get_normalizer('returns')
+                state_values = state_values * ret_std
+                next_state_values = next_state_values * ret_std
+
+            unnormalized_returns, advantages = self.compute_gae(state_values=state_values, next_state_values=next_state_values)
+
+            if 'returns' in self.normalizers:  # normalize returns and update statistics
+                self.update_normalizer('returns', unnormalized_returns)
+                _, ret_std = self.get_normalizer('returns')
+                returns = unnormalized_returns / ret_std
 
             np.random.shuffle(indexes)
             # training: iterate minibatches
@@ -185,7 +196,7 @@ class PPO(OnPolicyAgent):
                 batch_indexes = indexes[start:end]
                 mb_states = tf.gather(states, batch_indexes, axis=0)
                 mb_actions = tf.gather(actions, batch_indexes, axis=0)
-                mb_adv = tf.gather(adv, batch_indexes, axis=0)
+                mb_adv = tf.gather(advantages, batch_indexes, axis=0)
                 mb_returns = tf.gather(returns, batch_indexes, axis=0)
                 mb_logprobs = tf.gather(logprobs, batch_indexes, axis=0)
 
@@ -238,8 +249,8 @@ class PPO(OnPolicyAgent):
                         if self._gradient_clip_norm is not None:
                             critic_grads_log['critic/norm'] = critic_norm
                         self._log(do_log_step=False, prefix='gradients', **critic_grads_log)
-                    avg_state_value = np.mean(state_values.numpy())
-                    avg_td_return = np.mean(returns.numpy())
+                    avg_state_value = np.mean(tf.gather(state_values, batch_indexes, axis=0).numpy())
+                    avg_td_return = np.mean(tf.gather(unnormalized_returns, batch_indexes, axis=0).numpy())
                     self._log(do_log_step=False, prefix='debug', avg_state_value=avg_state_value, avg_td_return=avg_td_return)
                     self._log(do_log_step=True, critic_loss=loss_info['critic_loss'], train_step_v=self._train_step_v)
 